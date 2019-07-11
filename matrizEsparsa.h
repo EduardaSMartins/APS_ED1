ceb653e1c1abda4e988ceaecba2cfc4b3a935f53
EduardaSMartins <eduardamartins@alunos.utfpr.edu.br>
2019-07-11 10:52:11 -0300

    Matriz Esparsa

--- matrizEsparsa.h
+++ matrizEsparsa.h
@@ -1,5 +1,14 @@
-#include <stdlib.h>
+/************************************
+Algoritmos e Estrutura de Dados 1
+Prof. Rafael Liberato
+-----------------------------------
+Exercício: APS Matriz Esparsa
+Alunos: Caio Cintra, Eduarda Martins
+Data: 11/07/19
+*************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
 //VERSÃO COM SENTINELA
 typedef struct node{
 	int linha;
@@ -22,93 +31,173 @@ typedef struct{
 	//Quantidade de colunas da matriz
 } MatrizEsparsa;
 
-Node* criaSentinela();
-// --------------------- IMPLEMENTAÇÃO --------------------------- //
-//Cria uma matriz vazia e devolve seu endereço de memória.
-MatrizEsparsa *criarMatriz(int qtdeLinhas, int qtdeColunas){
-	//Aloca a matriz por linhas e colunas
-	MatrizEsparsa* m;
-	m = (MatrizEsparsa *)malloc(sizeof(MatrizEsparsa));
-	m->linhas = (Node **)calloc(qtdeLinhas, sizeof(Node*));
-	m->colunas = (Node **)calloc(qtdeColunas, sizeof(Node*));
-	for (int l = 0; l < qtdeLinhas; l++){
-		m->linhas[l] = criaSentinela();
-	}
-	for (int c = 0; c < qtdeLinhas; c++){
-		m->colunas[c] = criaSentinela();
-	}
-	return m;
-}
+// -------------------- PROTÓTIPOS DAS FUNÇÕES -------------------- //
 
-Node* criaSentinela(){
-	Node* novo = (Node*) malloc (sizeof(Node));
+Node *criaSentinela();
+MatrizEsparsa *criarMatriz(int qtdeLinhas, int qtdeColunas);
+Node *posicaoLinha(Node *sentinela, int coluna);
+int InsereLinha(MatrizEsparsa *m, int linha, Node *novo);
+Node *posicaoColuna(Node *sentinela, int linha);
+int InsereColuna(MatrizEsparsa *m, int coluna, Node *novo);
+int verifica(MatrizEsparsa *m, int linha, int coluna, int valor);
+int inserir(MatrizEsparsa *m, int linha, int coluna, int valor);
+int remover(MatrizEsparsa *m, int linha, int coluna);
+int acessar(MatrizEsparsa *m, int linha, int coluna);
+void imprimir(MatrizEsparsa *m);
+void desalocar(MatrizEsparsa *m);
+
+// --------------------- IMPLEMENTAÇÃO --------------------------- //
+
+Node *criaSentinela(){
+	Node *novo = (Node *)malloc(sizeof(Node));
 	novo->direita = novo->esquerda = novo;
 	novo->cima = novo->baixo = novo;
 	return novo;
 }
 
-int inserirEsquerda(Node* referencia, Node* novoNo){
-	novoNo->direita = referencia;
-	novoNo->esquerda = referencia->esquerda;
-	referencia->esquerda->direita = novoNo;
-	referencia->esquerda = novoNo;
-	if(novoNo->direita==referencia	&& novoNo->esquerda==referencia->esquerda)
-		return 1;
-	else
-		return -1;
-
+//Cria uma matriz vazia e devolve seu endereço de memória.
+MatrizEsparsa *criarMatriz(int qtdeLinhas, int qtdeColunas){
+	//Aloca a matriz por linhas e colunas
+	MatrizEsparsa *m;
+	m = (MatrizEsparsa *)malloc(sizeof(MatrizEsparsa));
+	m->linhas = (Node **)calloc(qtdeLinhas, sizeof(Node *));
+	m->colunas = (Node **)calloc(qtdeColunas, sizeof(Node *));
+	for (int l = 0; l < qtdeLinhas; l++){
+		m->linhas[l] = criaSentinela();
+	}
+	for (int c = 0; c < qtdeColunas; c++){
+		m->colunas[c] = criaSentinela();
+	}
+	m->numLinhas = qtdeLinhas;
+	m->numColunas = qtdeColunas;
+	return m;
 }
 
-int insereCima(Node* referencia, Node* novoNo){
-	novoNo->baixo = referencia;
-	novoNo->cima = referencia->cima;
-	referencia->cima->baixo = novoNo;
-	referencia->cima = novoNo;
-	if(novoNo->baixo==referencia && novoNo->cima->baixo==novoNo)
-		return 1;
-	else
-		return -1;
+
+Node *posicaoLinha(Node *sentinela, int coluna){
+
+	Node *aux = sentinela;
+	while (aux->direita != sentinela && coluna > aux->direita->coluna){
+		aux = aux->direita;
+	}
+	return aux;
 }
 
+//Função que insere o valor na linha desejada
+int InsereLinha(MatrizEsparsa *m, int linha, Node *novo){
+	Node *aux = posicaoLinha(m->linhas[linha], novo->coluna);
+
+	novo->direita = aux->esquerda;
+	novo->esquerda = aux->direita;
+	aux->direita->esquerda = novo;
+	aux->direita = novo->esquerda;
+
+	return 1;
+}
+
+//Funçao que devolve a posição da coluna desejada
+Node *posicaoColuna(Node *sentinela, int linha){
+	//
+	Node *aux = sentinela;
+	//
+	while (aux->baixo != sentinela && linha > aux->baixo->coluna){
+		aux = aux->baixo;
+	}
+	return aux;
+}
+
+//Função que insere o valor na coluna desejada
+int InsereColuna(MatrizEsparsa *m, int coluna, Node *novo){
+	//Recebe 
+	Node *aux = posicaoColuna(m->colunas[coluna], novo->linha);
+
+	novo->baixo = aux->cima;
+	novo->cima = aux->baixo;
+	aux->baixo->cima = novo;
+	aux->baixo = novo->cima;
+
+	return 1;
+}
 
 // Insere o <valor> na matriz <m> na linha <linha> e coluna <coluna>.
 // Caso a posição já exista, substitua o valor da célula.
+// Recebe o endereço da matriz, a linha e coluna nas quais o valor deve ser inserido
+int inserir(MatrizEsparsa *m, int linha, int coluna, int valor){
+	//Caso a matriz não esteja alocada ou a coluna na qual deseja inserir o valor
+	//seja maior ou menor do que a quantidade de colunas que a matriz possui, a função não 
+	//efetua nenhuma operação e retorna 0
+	if ((m == NULL) || (coluna > m->numColunas) || (coluna < 0))
+		return 0;
+	//Caso a matriz não esteja alocada ou a linha na qual deseja inserir o valor
+	//seja maior ou menor do que a quantidade de linhas que a matriz possui, a função não 
+	//efetua nenhuma operação e retorna 0
+	if ((m == NULL) || (linha > m->numLinhas) || (linha < 0))
+		return 0;
+	//Em casos válidos:
+	//Cria um nó do tipo sentinela
+	Node *novo = criaSentinela();
+	//Insere o valor no nó indicado (procura em qual linha ele pertence)
+	InsereLinha(m, linha, novo);
+	InsereColuna(m, coluna, novo);
+	return 1;
+}
 
-//int inserir(MatrizEsparsa *m, int linha, int coluna, int valor){
-//	Node* temp;
-
-//	return temp;
-//}
-
-int remover(MatrizEsparsa *m, int linha, int coluna);
+int remover(MatrizEsparsa *m, int linha, int coluna){
 // Remove o elemento da linha <linha> e coluna <coluna> na matriz <m> .
 // Devolva 1 caso a remoção seja efetivada e 0 caso contrário.
+	if(linha<0 || coluna<0 || linha>=m->numLinhas || coluna>=m->numColunas){
+        return 0;
+    }else{
+        Node* linhas = posicaoLinha(m->linhas[linha], coluna+1);
+        linhas->valor = 0;
+		if(m->linhas[linha]->coluna+1 == 0)
+			return 0;
+		else
+			return -1;
+    }
+}
 
-int acessar(MatrizEsparsa *m, int linha, int coluna);
-// Devolve o valor correspondente a linha e coluna solicitada.
-// Faça a validação dos índices. Caso a posição solicitada esteja fora do intervalo, devolva zero.
+
+int acessar(MatrizEsparsa *m, int linha, int coluna){
+	// Devolve o valor correspondente a linha e coluna solicitada.
+	// Faça a validação dos índices. Caso a posição solicitada esteja fora do intervalo, devolva zero.
+	if(linha<0 || coluna<0 || linha>=m->numLinhas || coluna>=m->numColunas){
+        return 0;
+    }else{
+        Node* linhas = posicaoLinha(m->linhas[linha], coluna+1);
+        return linhas->valor;
+    }
+}
 
 void imprimir(MatrizEsparsa *m){
-	int l,c;
-  	Node* aux;
-  	for(l = 0; l < m->numLinhas; l++) {    
-    	aux = m->linhas[l]->direita; 
-    for(c = 0; c < m->numColunas; c++){
-      	if(aux->coluna == c){
-        	printf("%03d ", aux->valor);
-        	aux = aux->direita;
-      	}else{
-        	printf("%03d ", 0);
-      }
-    }
-    printf("\n");
-  }   
-  printf("\n");
-
-}
 // Imprime os valores da matriz na tela. Cada linha deve ser impressa em uma linha diferente
 // e os elementos separados por espaço ou tabulação. Os elementos não representados na matriz (valor zero),
 // também devem ser impressos.
+	Node *aux;
+	for (int l = 0; l < m->numLinhas; l++){
+		aux = m->linhas[l]->direita;
+		for (int c = 0; c < m->numColunas; c++){
+			if (aux->coluna == c){
+				printf("%3d ", aux->valor);
+				aux = aux->direita;
+			}else{
+				printf("%3d ", 0);
+			}
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
 
-void desalocar(MatrizEsparsa *m);
-// Libera toda memória alocada dinamicamente para a matriz.
+void desalocar(MatrizEsparsa *m){
+	// Libera toda memória alocada dinamicamente para a matriz.
+	for (int l = 0; l < m->numLinhas; l++){
+		free(m->linhas[l]->direita);
+	}
+	for (int c = 0; c < m->numColunas; c++){
+		free(m->colunas[c]->direita);
+	}
+	free(m->linhas);
+	free(m->colunas);
+	free(m);
+}
